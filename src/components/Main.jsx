fd76658ff4bcddd7f69ba32656cf32224dfa43fd
bonfacenyatangi <bonfacenyatangi@gmail.com>
2023-12-11 22:52:30 +0300

    🎨 💄 Added bg image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-// import BgImage from "../assets/images/a.jpg";
+import BgImage from "../assets/images/a.jpg";
 const Main = () => {
   return (
     <div id="main">
       <img
-        className="w-full h-screen object-cover object-left scale-x-[-1]"
-        // src={BgImage}
+        className="w-full h-screen object-cover object-left scale-x-[1]"
+        src={BgImage}
         alt=""
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
         <div>
           <h1> I'm Bonface Nyatangi </h1>
-          <h2>I'm </h2>
+          <h2>I'm
+            
+          </h2>
         </div>
       </div>
     </div>
