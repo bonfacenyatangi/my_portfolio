166dadcefe4f4f0ffd8ad2f6227233a202d86701
bonfacenyatangi <bonfacenyatangi@gmail.com>
2023-12-11 14:18:38 +0300

    ⚡️ ✅ 🎨 Started  my portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidenav from "./components/Sidenav";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <Sidenav />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import BgImage from "../assets/images/a.jpg";
+const Main = () => {
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
+        src={BgImage}
+        alt=""
+      />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div>
+          <h1> I'm Bonface Nyatangi </h1>
+          <h2>I'm </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
