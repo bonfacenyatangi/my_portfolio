07ed35fb2a91437876cd20b77512e0bdad71d8f5
bonfacenyatangi <bonfacenyatangi@gmail.com>
2023-12-11 23:38:36 +0300

    💄 Updated the typed animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,27 +16,31 @@ const Main = () => {
         alt=""
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div className="">
-          <h1> I'm Bonface Nyatangi </h1>
-          <h2>
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I'm Bonface Nyatangi
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
             I'm a
-            <br />
-            <span></span>
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                "Developer",
+                "Software Developer",
                 2000, // wait 1s before replacing "Mice" with "Hamsters"
                 "Tech Enthusiast",
                 2000,
-                "Coder",
+                "UI/UX Designer",
                 2000,
                 "Peasant Farmer",
                 2000,
               ]}
               wrapper="span"
               speed={50}
-              style={{ fontSize: "1em", display: "inline-block" }}
+              style={{
+                fontSize: "1em",
+                display: "inline-block",
+                paddingLeft: "7px",
+              }}
               repeat={Infinity}
             />
           </h2>
