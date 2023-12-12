261bec545dcfc7ba4fec7f366863ed122eaaf0fb
bonfacenyatangi <bonfacenyatangi@gmail.com>
2023-12-12 20:34:18 +0300

    🎨 ⚡️ 💄 Implemented work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,9 +12,8 @@ const data = [
     year: "2022, January - Decemeber ",
     title: "ICT Support Intern",
     duration: "1 year",
-    details: {
-      responsibilities: "Offering essential ICT Services for the Authority",
-    },
+    details:
+      "Resolved 200+ user incidents and met 95% of SLAs annually. Deployed IT equipment for 30+ staff, improving productivity. Installed/updated 50+ software applications for organization-wide efficiency. Managed an inventory of 1500+ IT assets and tracked changes. Provided timely hardware/software maintenance to minimize downtime. Supported virtual meeting solutions for 500+ employees. Completed additional tasks assigned by supervisors to support the team and organization.",
   },
   {
     year: "2021, July - Decemeber ",
