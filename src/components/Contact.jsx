3ee4e8c730b8b1b7771de9ed5be82ffd45b755c3
bonfacenyatangi <bonfacenyatangi@gmail.com>
2023-12-13 20:31:21 +0300

    ✨ Created contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,25 +10,49 @@ const Contact = () => {
         <div className="grid md:grid-cols-2 gap-4 w-full py-2 ">
           <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Name</label>
-            <input type="text" name="name" />
+            <input
+              className="border-2 rounded-lg p-3 flex border-green-300"
+              type="text"
+              name="name"
+            />
           </div>
-          <div>
-            <label className="uppercase text-sm py-2">Phone Number</label>
-            <input type="text" name="phone" id="" />
+          <div className="flex flex-col">
+            <label className="uppercase text-sm py-2">Phone</label>
+            <input
+              className="border-2 rounded-lg p-3 flex border-green-300"
+              type="text"
+              name="phone"
+              id=""
+            />
           </div>
         </div>
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Email</label>
-          <input type="email" name="email" />
+          <input
+            className="border-2 rounded-lg p-3 flex border-green-300"
+            type="email"
+            name="email"
+          />
         </div>
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Subject</label>
-          <input type="text" name="email" />
+          <input
+            className="border-2 rounded-lg p-3 flex border-green-300"
+            type="text"
+            name="email"
+          />
         </div>
-        <div>
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm py-2">Message</label>
-          <textarea rows="10" name="message"></textarea>
+          <textarea
+            className="border-2 rounded-lg p-3 flex border-green-300"
+            rows="10"
+            name="message"
+          ></textarea>
         </div>
+        <button className="bg-[#01890f] text-gray-100 mt-4 w-full p-4 rounded-lg">
+          Let's Talk
+        </button>
       </form>
     </div>
   );
